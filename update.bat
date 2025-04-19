3a699e6f5ac5825eef22a526efc210237bdf1d40
fallingheaven <296142289@qq.com>
2025-04-19 23:53:28 +0800

    update content

--- update.bat
+++ update.bat
@@ -1,14 +1,17 @@
 @echo off
 REM 自动化 Hugo 博客内容的提交与推送
 
+REM 设置命令行编码为 UTF-8
+chcp 65001
+
 REM 步骤1：添加所有更改
 git add .
 
 REM 步骤2：输入提交信息
-set /p msg=请输入提交说明（默认：更新内容）:
+set /p msg=请输入提交说明（默认：update content）:
 
 if "%msg%"=="" (
-    set msg=更新内容
+    set msg=update content
 )
 
 REM 步骤3：提交
