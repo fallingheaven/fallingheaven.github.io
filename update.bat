9cd1611cf8ec20e9d05d79477751529df69ce371
fallingheaven <296142289@qq.com>
2025-04-19 23:58:15 +0800

    补充提交md测试文件

--- update.bat
+++ update.bat
@@ -7,17 +7,20 @@ chcp 65001
 REM 步骤1：添加所有更改
 git add .
 
-REM 步骤2：输入提交信息
+REM 步骤2：生成静态文件
+hugo --minify
+
+REM 步骤3：输入提交信息
 set /p msg=Please enter commit message (default: update content):
 
 if "%msg%"=="" (
     set msg=update content
 )
 
-REM 步骤3：提交
+REM 步骤4：提交
 git commit -m "%msg%"
 
-REM 步骤4：推送到远程 main 分支
+REM 步骤5：推送到远程 main 分支
 git push origin main
 
 pause
